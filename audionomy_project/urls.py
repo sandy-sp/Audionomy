8f8ca004d5b8e87e7f3db599e9da80c7c89dfbe2
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-04 15:57:14 -0500

    Fix NameError in urls.py by importing path and include

--- audionomy_project/urls.py
+++ audionomy_project/urls.py
@@ -1,63 +1,28 @@
 """
-URL configuration for audionomy_project project.
+URL configuration for the Audionomy project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
+This file routes URLs to views. It includes:
+  - The Django admin site.
+  - The URL patterns defined in the audionomy_app application.
+  - Automatic serving of media files during development.
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('audionomy_app.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-"""
-URL configuration for the Audionomy Project.
-
-This file defines the top-level URL routing for the Django project named
-'audionomy_project'. It includes:
-
-- Admin site routes (for Django's admin panel).
-- Application routes from 'audionomy_app.urls'.
-- Automatic serving of media files during development (if DEBUG=True).
-
-Reference:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-    https://docs.djangoproject.com/en/4.2/howto/static-files/#serving-files-uploaded-by-a-user
+For more details see:
+  - https://docs.djangoproject.com/en/4.2/topics/http/urls/
+  - https://docs.djangoproject.com/en/4.2/howto/static-files/#serving-files-uploaded-by-a-user
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # <--- Added 'path' and 'include' imports
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # Django admin route:
+    # Admin route
     path('admin/', admin.site.urls),
-    # Include our custom app's URLs:
+    # Include audionomy_app URLs
     path('', include('audionomy_app.urls')),
 ]
 
 if settings.DEBUG:
-    """
-    In development mode, serve user-uploaded media files from MEDIA_URL
-    using Django’s built-in static file server.
-
-    WARNING:
-        In production, do NOT use this for serving media; configure a proper
-        media file host or a cloud service. 
-    """
+    # In development, serve media files from MEDIA_URL using Django's static file server.
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

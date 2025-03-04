f2d68457639e9c79eaafdf3e2922ae958535c010
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-04 15:27:31 -0500

    Refine admin.py with docstrings & improved admin configurations

--- audionomy_app/admin.py
+++ audionomy_app/admin.py
@@ -1,10 +1,47 @@
+"""
+Django Admin configuration for the audionomy_app.
+
+This file defines how the Dataset and AudioEntry models appear and behave
+in the Django admin site, letting administrators view, search, and manage records.
+
+Reference:
+    https://docs.djangoproject.com/en/4.2/ref/contrib/admin/
+"""
+
 from django.contrib import admin
 from .models import Dataset, AudioEntry
 
+
 @admin.register(Dataset)
 class DatasetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at')
+    """
+    Customize how Dataset objects are displayed in the admin.
+    - Shows 'name' and 'created_at' in the list view.
+    - Allows searching by 'name'.
+    - Provides a date hierarchy based on 'created_at' if desired.
+    """
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+    date_hierarchy = "created_at"
+    ordering = ("-created_at",)
+
 
 @admin.register(AudioEntry)
 class AudioEntryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'dataset')
+    """
+    Customize how AudioEntry objects are displayed in the admin.
+    - Lists title, dataset, durations, and creation date.
+    - Enables searching by 'title'.
+    - Allows filtering by 'dataset'.
+    """
+    list_display = (
+        "title",
+        "dataset",
+        "audio1_duration",
+        "audio2_duration",
+        "created_at"
+    )
+    search_fields = ("title",)
+    list_filter = ("dataset",)
+    date_hierarchy = "created_at"
+    ordering = ("-created_at",)

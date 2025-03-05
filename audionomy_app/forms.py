dfe14b771508f3cc8c11511f4b8f8faadd2bbcd5
sandy-sp <sandeep.paidipati@gmail.com>
2025-03-05 06:58:29 -0500

    updated forms.py script

--- audionomy_app/forms.py
+++ audionomy_app/forms.py
@@ -4,4 +4,15 @@ from .models import AudioEntry
 class AudioEntryForm(forms.ModelForm):
     class Meta:
         model = AudioEntry
-        fields = ["title", "style_prompt", "exclude_style_prompt", "model_used", "audio_file_1", "audio_file_2"]
+        fields = [
+            "title",
+            "style_prompt",
+            "exclude_style_prompt",
+            "model_used",
+            "youtube_link",
+            "lyrics",
+            "persona",
+            "uploaded_sample",
+            "audio_file_1",
+            "audio_file_2"
+        ]
